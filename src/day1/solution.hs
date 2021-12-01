1ee05d79ff3d11d9fe8355570dacb4c8e9749a34
Matt McElwee <mmcelwee@dmsi.com>
2021-12-01 16:25:54 -0600

    Solved part two

--- src/day1/solution.hs
+++ src/day1/solution.hs
@@ -1,6 +1,9 @@
 module Main where
 import System.IO
 import Control.Monad
+import Control.Applicative
+import Data.Traversable (sequenceA)
+import Data.List (tails)
 
 mapIdx :: (a -> Int -> b) -> [a] -> [b]
 mapIdx f l = zipWith f l [0..]
@@ -18,11 +21,26 @@ findIncrease list = filter isTrue (mapIdx (prevHasIncrease list) list)
 f :: [String] -> [Int]
 f = map read
 
+partOne :: [Int] -> Int
+partOne list = length (findIncrease list)
+
+-- partTwo runs partOne after summing
+
+transpose' :: [[a]] -> [[a]]
+transpose' = getZipList . sequenceA . map ZipList
+
+windows :: Int -> [a] -> [[a]]
+windows m = transpose' . take m . tails
+
+partTwo :: [Int] -> Int
+partTwo list = partOne (map sum (windows 3 list))
+
 main = do 
     let list = []
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let singleWords = words contents
         list = f singleWords
-    print (length (findIncrease list))
+    print ("Part 1: " ++ show (partOne list))
+    print ("Part 2: " ++ show (partTwo list))
     hClose handle
